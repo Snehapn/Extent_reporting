4075b62c2aa17a03bc337e38158f16f1ae133339
sneha <snehanatekar@whitehatjr.com>
2021-07-05 01:35:03 +0530

    actions and frames

---
+++ Section12/src/WindowsHandle.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowsHandle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/loginpagePractise//");
+		driver.findElement(By.cssSelector("a[class='blinkingText']")).click();
+		
+		//switch from parent to child browser
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		String parentId = it.next();
+		String childId = it.next();
+		driver.switchTo().window(childId);
+		System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
+		//System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText().split(" ")[4].trim());
+		String emailId = driver.findElement(By.cssSelector(".im-para.red")).getText().split("at")[1].trim().split(" ")[0];
+		driver.switchTo().window(parentId);
+		driver.findElement(By.id("username")).sendKeys(emailId);
+		
+	}
+
+}
