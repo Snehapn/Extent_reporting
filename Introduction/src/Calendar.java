83f5cef0a9501bf3f7b6d314006a32fb2bc4b66c
sneha <snehanatekar@whitehatjr.com>
2021-07-07 01:09:31 +0530

    dynamic calendar changes

--- Introduction/src/Calendar.java
+++ Introduction/src/Calendar.java
@@ -10,25 +10,32 @@ public class Calendar {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");	
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://www.path2usa.com/travel-companions"); 
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.path2usa.com/travel-companions");
 		Thread.sleep(5000);
-		driver.findElement(By.id("travel_date")).click();
-		
+		driver.findElement(By.xpath("//*[@id='travel_date']")).click();
+		WebElement selectMonth = driver
+				.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']"));
+
+		while (!selectMonth.getText().contains("May")) {
+
+			driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+		}
+
 		List<WebElement> dates = driver.findElements(By.className("day"));
 		int count = driver.findElements(By.className("day")).size();
-		
-		for (int i=0; i<count; i++) {
-			
-		String text = driver.findElements(By.className("day")).get(i).getText();
-		
-		if(text.equalsIgnoreCase("23")) {
-			driver.findElements(By.className("day")).get(i).click();
-			break;
+
+		for (int i = 0; i < count; i++) {
+
+			String text = driver.findElements(By.className("day")).get(i).getText();
+
+			if (text.equalsIgnoreCase("21")) {
+				driver.findElements(By.className("day")).get(i).click();
+				break;
 			}
 		}
-		
+
 	}
 
 }
